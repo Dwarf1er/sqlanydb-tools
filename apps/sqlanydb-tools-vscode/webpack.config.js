898d802eb5955b1fb3dd2a16ceb9c3bfc2b82c20
Dwarf1er <SerolTech@protonmail.com>
2024-10-23 22:58:21 -0400

    Fixed turborepo configuration

--- apps/sqlanydb-tools-vscode/webpack.config.js
+++ apps/sqlanydb-tools-vscode/webpack.config.js
@@ -3,51 +3,28 @@
 "use strict";
 
 const path = require("path");
-const fs = require("fs");
-
-const getInternalPackages = (dir) => {
-	return fs
-		.readdirSync(dir)
-		.filter((item) => {
-			const itemPath = path.join(dir, item);
-			return fs.statSync(itemPath).isDirectory() && item !== "sqlanydb-tools-vscode";
-		})
-		.map((item) => path.join(dir, item, "src", "index.ts"));
-};
-
-const packagesDir = path.resolve(__dirname, "../../packages");
-const appsDir = path.resolve(__dirname, "../../apps");
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
 const extensionConfig = {
-	target: "node",
-	mode: "none",
+	target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+	mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-	entry: {
-		extension: "./src/extension.ts",
-		...getInternalPackages(packagesDir).reduce((acc, packageEntry) => {
-			const packageName = path.basename(path.dirname(packageEntry));
-			acc[packageName] = packageEntry;
-			return acc;
-		}, {}),
-		...getInternalPackages(appsDir).reduce((acc, appEntry) => {
-			const appName = path.basename(path.dirname(appEntry));
-			acc[appName] = appEntry;
-			return acc;
-		}, {}),
-	},
+	entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
 	output: {
+		// the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
 		path: path.resolve(__dirname, "dist"),
-		filename: "[name].js",
+		filename: "extension.js",
 		libraryTarget: "commonjs2",
 	},
 	externals: {
-		vscode: "commonjs vscode",
+		vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+		// modules added here also need to be added in the .vscodeignore file
 	},
 	resolve: {
+		// support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
 		extensions: [".ts", ".js"],
 	},
 	module: {
@@ -65,7 +42,7 @@ const extensionConfig = {
 	},
 	devtool: "nosources-source-map",
 	infrastructureLogging: {
-		level: "log",
+		level: "log", // enables logging required for problem matchers
 	},
 };
 module.exports = [extensionConfig];
